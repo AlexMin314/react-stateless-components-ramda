1829d492b90ed02989aa4ade9332a3410c1517a0
Mirko Mariani <mariani.mirko@gmail.com>
2016-07-04 18:20:40 +0200

    Full source code for the article

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
-var ReactDOM = require('react-dom');
-var R = require('ramda');
+const ReactDOM = require('react-dom');
+const R = require('ramda');
 
 module.exports = R.curry((element, component, props) => {
   return ReactDOM.render(component(props), element);
